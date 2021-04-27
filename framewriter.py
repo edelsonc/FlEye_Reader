4dd97353a005c58bfcc9597cb37a4bea179996e8
cedelson <cedelson@iu.edu>
2021-04-27 15:03:52 -0400

    Created file for FrameWriter and corresponding test file

---
+++ framewriter.py
@@ -0,0 +1,6 @@
+class FrameWriter(object):
+    """
+    Class for validating and writing individual data frames from FlEye camera.
+    """
+    pass
+
---
+++ test_framewriter.py
@@ -0,0 +1,6 @@
+import pytest
+from framewriter import FrameWriter
+
+def test_FrameWriter_initialization():
+    framewriter = FrameWriter()
+
