1638967418f13c8e1bfe2485925566553bf274d3
Charlie Edelson <cedelson@pop-os.localdomain>
2021-04-27 10:45:58 -0400

    Basic chunker structure coming together - `Chunker` init tests written - `Chunker` __init__ finished

--- chunker.py
+++ chunker.py
@@ -1,4 +1,26 @@
 class Chunker(object):
-    pass
+    """
+    Class used to process raw camera stream binary data. `Chunker` breaks the
+    file into individual chunks of fixed block size to avoid having all of the
+    data in memory at once. Additionally, `Chunker` overlaps the chunks by a
+    fixed number of blocks to ensure a camera frame isn't accidentally skipped
+    in processing. It's action can be visualized as below
 
-
+    [BLOCK BLOCK BLOCK BLOCK] <- Chunk 1
+                      [BlOCK BLOCK BLOCK BLOCK] <- Chunk 2
+                                        [BlOCK BLOCK BLOCK BLOCK] <- Chunk 3
+                                            ...
+    Arguments
+    ---------
+    data_file -- file name for the file to be read and chunked using chunker
+    block_size -- size of individual blocks to be read in chunker
+    block_number -- number of blocks to be read in each chunk
+    chunk_overlap -- number of blocks that should overlap in each chunk
+    """
+    def __init__(self, data_file, block_size = 512, block_number = 10, chunk_overlap = 1):
+        """create instance of `Chunker` with sensible default values"""
+        self.block_size = block_size
+        self.block_number = block_number
+        self.chunk_overlap = chunk_overlap
+        self.file = data_file
+        self.file_object = open(data_file, "rb") 
--- test_chunker.py
+++ test_chunker.py
@@ -1,10 +1,27 @@
 import pytest
+import tempfile
+from random import getrandbits
 from chunker import Chunker
 
-def test_default_initial_arguments():
-    chunker = Chunker()
+@pytest.fixture
+def read_tempfile():
+    """Create a temporary file with random bytes"""
+    t = tempfile.NamedTemporaryFile(delete=False)
+    t.write(bytes([getrandbits(8) for _ in range(1024 * 50)]))
+    return t       
+
+def test_initialization(read_tempfile):
+    chunker = Chunker(read_tempfile.name)
+    assert chunker.file == read_tempfile.name
+    assert chuner.file_object == read_tempfile
     assert chunker.block_size == 512
-    assert chunker.block_number == 2
+    assert chunker.block_number == 10
     assert chunker.chunk_overlap == 512
+
+    with pytest.raises(TypeError):
+        chunker = Chunker()
+
+    with pytest.raises(FileNotFoundError):
+        chunker = Chunker("~/cats_the_musical.bin")
 
 
