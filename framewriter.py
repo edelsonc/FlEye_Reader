ee904993b3327abae99fbafbc8bc6a583fa23840
cedelson <cedelson@iu.edu>
2021-04-27 15:50:27 -0400

    Created basic FrameWriter __init__ - Added __init__ and tests

--- framewriter.py
+++ framewriter.py
@@ -2,5 +2,8 @@ class FrameWriter(object):
     """
     Class for validating and writing individual data frames from FlEye camera.
     """
-    pass
+    def __init__(self, write_file, frame_length = 1012):
+        self.frame_length = frame_length
+        self.write_file = write_file
+        self.write_file_object = open(write_file, "wb")
 
--- test_framewriter.py
+++ test_framewriter.py
@@ -1,6 +1,31 @@
 import pytest
+import tempfile
 from framewriter import FrameWriter
 
-def test_FrameWriter_initialization():
-    framewriter = FrameWriter()
+@pytest.fixture
+def test_frames():
+    """
+    Use binary file created by `create_test_file.py` to simulate 100 frame of
+    random camera data. 
+    """
+    with open("Data/testy.bin", 'rb') as f:
+        data = f.read()
+    return data.split(b'\xbf' * 12)
 
+
+@pytest.fixture
+def write_file():
+    """
+    Creates a temporary write file for use with FrameWriter
+    """
+    t = tempfile.NamedTemporaryFile()
+    return t
+
+def test_FrameWriter_init(write_file):
+    framewriter = FrameWriter(write_file.name)
+
+    assert framewriter.frame_length == 1012
+    assert framewriter.write_file == write_file.name
+
+    with pytest.raises(TypeError):
+        framewriter = FrameWriter()
