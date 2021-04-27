1e4d03f408b1ef59ffe1dada054a9fb31d7e7aa3
Charlie Edelson <cedelson@pop-os.localdomain>
2021-04-27 11:48:27 -0400

    Chunker next_chunk method implemented - Rewinds the file so that you have overlaps - Returns `b""` at end of file

--- chunker.py
+++ chunker.py
@@ -13,14 +13,47 @@ class Chunker(object):
     Arguments
     ---------
     data_file -- file name for the file to be read and chunked using chunker
-    block_size -- size of individual blocks to be read in chunker
+    block_size -- size in bytes of individual blocks to be read in chunker
     block_number -- number of blocks to be read in each chunk
     chunk_overlap -- number of blocks that should overlap in each chunk
     """
-    def __init__(self, data_file, block_size = 512, block_number = 10, chunk_overlap = 1):
-        """create instance of `Chunker` with sensible default values"""
+    def __init__(self, data_file, block_size = 512, block_number = 10, chunk_overlap = 2):
+        """
+        Create instance of `Chunker` with sensible default values
+        """
         self.block_size = block_size
         self.block_number = block_number
         self.chunk_overlap = chunk_overlap
         self.file = data_file
         self.file_object = open(data_file, "rb") 
+
+    def next_chunk(self):
+        """
+        Read method that returns chunks. This function is responsible for both
+        reading in chunks of the data file and setting the read position back
+        to get overlap in chunks.
+
+        When the end of the file is hit `next_chunk` will return the empty
+        string, ''. Currently `EOFError` is not raised to allow use in the
+        following way:
+
+        data == None
+        while data != "":
+            do things with reading chunks
+
+        An alternative using `EOFError` would be
+
+        while True:
+            try:
+                data = chunker.next_chunk()
+
+            except EOFError:
+                break
+        """
+        data = self.file_object.read(self.block_size * self.block_number)
+        
+        if len(data) == self.block_size * self.block_number:
+            self.file_object.seek(-self.block_size * self.chunk_overlap, 1)
+        
+        return data
+
--- test_chunker.py
+++ test_chunker.py
@@ -4,19 +4,29 @@ from random import getrandbits
 from chunker import Chunker
 
 @pytest.fixture
-def read_tempfile():
+def read_tmpfile():
     """Create a temporary file with random bytes"""
-    t = tempfile.NamedTemporaryFile(delete=False)
+    t = tempfile.NamedTemporaryFile()
     t.write(bytes([getrandbits(8) for _ in range(1024 * 50)]))
+    t.seek(0)
     return t       
 
-def test_initialization(read_tempfile):
-    chunker = Chunker(read_tempfile.name)
-    assert chunker.file == read_tempfile.name
-    assert chuner.file_object == read_tempfile
+
+@pytest.fixture
+def small_tmpfile():
+    """Creates a small temporary file with random bytes"""
+    t = tempfile.NamedTemporaryFile()
+    t.write(bytes([getrandbits(8) for _ in range(1024 * 5)]))
+    t.seek(0)
+    return t
+ 
+
+def test_initialization(read_tmpfile):
+    chunker = Chunker(read_tmpfile.name)
+    assert chunker.file == read_tmpfile.name
     assert chunker.block_size == 512
     assert chunker.block_number == 10
-    assert chunker.chunk_overlap == 512
+    assert chunker.chunk_overlap == 2
 
     with pytest.raises(TypeError):
         chunker = Chunker()
@@ -24,4 +34,35 @@ def test_initialization(read_tempfile):
     with pytest.raises(FileNotFoundError):
         chunker = Chunker("~/cats_the_musical.bin")
 
+
+def test_basic_read(read_tmpfile):
+    # read in content of temp file to check results
+    tmpfile_content = read_tmpfile.read()
+    read_tmpfile.seek(0)
+
+    # create chunker and read first chunk
+    chunker = Chunker(read_tmpfile.name)
+    data = chunker.next_chunk()
+    assert len(data) == chunker.block_size * chunker.block_number
+    assert data == tmpfile_content[0:(512*10)]
+
+    # read another chunk and check it too
+    data2 = chunker.next_chunk()
+    assert data2 == tmpfile_content[512*8:512*18]
+
+
+def test_end_of_file(small_tmpfile):
+    tmpfile_content = small_tmpfile.read()
+    small_tmpfile.seek(0)
+
+    chunker = Chunker(small_tmpfile.name)
+    data1 = chunker.next_chunk()
+    data2 = chunker.next_chunk()
+    data3 = chunker.next_chunk()
+    data4 = chunker.next_chunk()
+
+    assert data1 == tmpfile_content
+    assert data2 == tmpfile_content[-1024:]
+    assert data3 == b""
+    assert data4 == b""
 
