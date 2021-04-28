c37253886a5cc08748164e21878f5bf2bb59c4ea
cedelson <cedelson@iu.edu>
2021-04-28 14:31:56 -0400

    Things...

--- framewriter.py
+++ framewriter.py
@@ -9,6 +9,7 @@ class FrameWriter(object):
         header/delimiter
     frame_header -- header for each frame used to split frames in earlier step
     frame_footer -- footer for each frame if present
+    frame_spacers -- a list of tuples of the form (spacer start, spacer string)
     """
     def __init__(self, write_file, frame_header, frame_footer=None, frame_spacers=None, frame_length = 1024):
         self.frame_length = frame_length
@@ -33,11 +34,23 @@ class FrameWriter(object):
         ---------
         frame -- a raw binary frame from the FlEye camera with header removed
         """
+        # check frame is correct length
         if (len(frame) + len(self.frame_header)) != self.frame_length:
             return False
 
+        # If footer is provided check that frame has appropriate footer
         if (self.frame_footer != None):
             footer_length = len(self.frame_footer)
             if frame[-footer_length:] != self.frame_footer:
+                return False
+
+        # If spacers exist in the frame ensure that they occur at the correct
+        # places
+        if (self.frame_spacers != None):
+            spacer_end = [spacer[0] + len(spacer[1]) for spacer in self.frame_spacers]
+            valid_spacers = []
+            for i, spacer in enumerate(self.frame_spacers):
+                valid_spacers.append(frame[spacer[0]:spacer_end[i]] == spacer[1])
+            if not all(valid_spacers):
                 return False
 
