a33abe964ea279bdea8b55cc2bf7386b803f9f31
cedelson <cedelson@iu.edu>
2021-06-11 09:17:04 -0400

    Fixing errors with use of tempfile - Replacing tempfile usage with pyste tmpdir

--- test_chunker.py
+++ test_chunker.py
@@ -1,29 +1,13 @@
 import pytest
-import tempfile
-from random import getrandbits
 from chunker import Chunker
+from test_helpers import random_bytes, small_random_bytes
 
-@pytest.fixture
-def read_tmpfile():
-    """Create a temporary file with random bytes"""
-    t = tempfile.NamedTemporaryFile()
-    t.write(bytes([getrandbits(8) for _ in range(1024 * 50)]))
-    t.seek(0)
-    return t       
+def test_initialization(tmpdir):
+    read_tmpfile = tmpdir.join("test_init.bin")
+    tmpfile_content = random_bytes(read_tmpfile)
 
-
-@pytest.fixture
-def small_tmpfile():
-    """Creates a small temporary file with random bytes"""
-    t = tempfile.NamedTemporaryFile()
-    t.write(bytes([getrandbits(8) for _ in range(1024 * 5)]))
-    t.seek(0)
-    return t
- 
-
-def test_initialization(read_tmpfile):
-    chunker = Chunker(read_tmpfile.name)
-    assert chunker.file == read_tmpfile.name
+    chunker = Chunker(read_tmpfile)
+    assert chunker.file == read_tmpfile
     assert chunker.block_size == 512
     assert chunker.block_number == 10
     assert chunker.chunk_overlap == 2
@@ -38,13 +22,12 @@ def test_Chunker_incorrect_arguments():
         chunker = Chunker("~/cats_the_musical.bin")
 
 
-def test_basic_read(read_tmpfile):
-    # read in content of temp file to check results
-    tmpfile_content = read_tmpfile.read()
-    read_tmpfile.seek(0)
+def test_basic_read(tmpdir):
+    read_tmpfile = tmpdir.join("test.bin")
+    tmpfile_content = random_bytes(read_tmpfile)
 
     # create chunker and read first chunk
-    chunker = Chunker(read_tmpfile.name)
+    chunker = Chunker(read_tmpfile)
     chunk_id, data, byte_loc = chunker.next_chunk()
     assert chunk_id == 0
     assert byte_loc == 0
@@ -58,11 +41,11 @@ def test_basic_read(read_tmpfile):
     assert data2 == tmpfile_content[512*8:512*18]
 
 
-def test_end_of_file(small_tmpfile):
-    tmpfile_content = small_tmpfile.read()
-    small_tmpfile.seek(0)
+def test_end_of_file(tmpdir):
+    small_tmpfile = tmpdir.join("test_OEF.bin")
+    tmpfile_content = small_random_bytes(small_tmpfile)
 
-    chunker = Chunker(small_tmpfile.name)
+    chunker = Chunker(small_tmpfile)
     chunk_id1, data1, byte_loc1 = chunker.next_chunk()
     chunk_id2, data2, byte_loc2 = chunker.next_chunk()
     chunk_id3, data3, byte_loc3 = chunker.next_chunk()
@@ -84,9 +67,12 @@ def test_end_of_file(small_tmpfile):
     assert byte_loc4 == len(tmpfile_content)
 
 
-def test_close_file(read_tmpfile):
+def test_close_file(tmpdir):
     # create a `Chunker` object and read some random data
-    chunker = Chunker(read_tmpfile.name)
+    read_tmpfile = tmpdir.join("test_close.bin")
+    tmpfile_content = random_bytes(read_tmpfile)
+
+    chunker = Chunker(read_tmpfile)
     chunk_id, data, byte_loc = chunker.next_chunk()
 
     # now we close the file and check that a new read raises an error
