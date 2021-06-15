6f9f60936a5f77abd5e9c4b9183867819851b361
Edelson <cedelson@iu.edu>
2021-06-15 16:06:49 -0400

    Push last few things I'm working on

--- FlEye_Reader.py
+++ FlEye_Reader.py
@@ -5,15 +5,23 @@ from framewriter import FrameWriter
 
 
 if __name__ == "__main__":
-    
+   
+    # begin by grabbing the read file for the data and write file 
     if len(sys.argv) >= 3:
         read_file, write_file = sys.argv[1], sys.argv[2]
     else:
         print("Missing required commandline arguments: read_file and write_file")
         sys.exit(1)
 
+    # download the configurations for the various validators and readers
+    with open("configs.txt", "r") as f:
+        configs = [config.strip() for config in f.readlines()]
+
+    #  creater an instance of Chunker with the read_file
     try:
         chunker = Chunker(read_file)
     except FileNotFoundError:
         print("Provided read_file cannot be open. Check that the path to the read_file is correct")
         sys.exit(1)
+
+
