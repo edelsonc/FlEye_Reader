da9e155df8aaceb448c68e5a36f001aee48987de
Edelson <cedelson@iu.edu>
2021-06-16 10:59:32 -0400

    Updating documentation on FrameWriter

--- framewriter.py
+++ framewriter.py
@@ -7,9 +7,13 @@ class FrameWriter(object):
 
     Arguments
     ---------
+    unpack_string -- string used to unpack the raw byte data into numerical
+        types; as specified at https://docs.python.org/3/library/struct.html#format-strings
     write_file -- file that parsed and validated frames are writen to
     log_file -- file for logging FrameWriter initialization and any issues
         writing frames
+    block_size -- size of data blocks you will write. FrameWriter will pad
+        reformatted data to have this many bytes.
     """
     def __init__(self, unpack_string, write_file, log_file, block_size = 1024):
         self.unpack_string = unpack_string
@@ -22,6 +26,14 @@ class FrameWriter(object):
         logging.debug("FrameWriter initialized")
 
     def write(self, frame):
+        """
+        Public facing fuction used to reformatted and write a frame to the
+        FrameWriter.write_file
+
+        Arguments
+        ---------
+        frame -- fly camera frame validated by FrameValidator
+        """
         try:
             reformatted_frame = self._reformat_frame(frame)
             self.write_file_object.write(reformatted_frame)
@@ -30,14 +42,25 @@ class FrameWriter(object):
             logging.exception("Error occured while writing Frame {}".format(frame_id))
 
     def close(self):
+        """
+        Public facing function to close the write file after processing a data
+        file is complete
+        """
         self.write_file_object.close()
         logging.debug("FrameWriter write file closed")
 
     def _reformat_frame(self, frame):
         """
-        take a frame -> break it into its 'sections' -> cast those to 4 byte
-        numbers (add \x00 to each one until it's good) -> write these in write
-        file -> log errors 
+        Function to parse a data frame, organize the pixels by id, and 
+        reformatted/pad all numbers to be 4 bytes
+
+        NOTE: IMU data should be treated as 16 bit unsigned integers. For these
+        values the writing to 4 bytes should be thought of as adding two
+        padding bytes of the format 0x00.
+
+        Arguments
+        ---------
+        frame -- fly camera frame validated by FrameValidator
         """
         # break frame into each individual piece of "data"
         frame_tuple = self._unpack(frame)
@@ -63,14 +86,14 @@ class FrameWriter(object):
 
     def _unpack(self, frame):
         """
-        Function used to take a raw validated frame and break it into its
+        Private function used to take a raw validated frame and break it into its
         various components
         """
         return struct.unpack(self.unpack_string, frame)
 
     def _order_adc(self, adc_tuple):
         """
-        Function to take raw adc data and order it by pixel id. Additionally
+        Private function to take raw adc data and order it by pixel id. Additionally
         casts to 32 bit
         """
         adc32 = []
