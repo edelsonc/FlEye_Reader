b480508f9d78e3ab9bec87ef8a8555e8ca75ed92
Edelson <cedelson@iu.edu>
2021-06-18 11:06:57 -0400

    Updated FlEye_Reader to have a CLI

--- FlEye_Reader.py
+++ FlEye_Reader.py
@@ -1,4 +1,5 @@
 import sys, os
+import click
 from pathlib import Path
 from datetime import datetime
 from chunker import Chunker
@@ -6,7 +7,7 @@ from framevalidator import FrameValidator
 from framewriter import FrameWriter
 from configurations import configs
 
-def create_log_paths(read_file, write_file):
+def create_log_paths(read_file):
     """
     Helper function to setup two log files for the current run of the data
     reading script.
@@ -14,23 +15,17 @@ def create_log_paths(read_file, write_file):
     logging_dir = Path(os.getcwd()) / "Log"
     
     read_file_tail = os.path.split(read_file)[1]
-    write_file_tail = os.path.split(write_file)[1]
 
     current_time_str = datetime.now().strftime("%m_%d_%y-%H-%M-%S")
     read_log = logging_dir / (read_file_tail + current_time_str + ".log")
-    write_log = logging_dir / (write_file_tail + current_time_str + ".log")
-    return read_log, write_log 
+    return read_log
 
 
-if __name__ == "__main__":
-   
-    # begin by grabbing the read file for the data and write file 
-    if len(sys.argv) >= 3:
-        read_file, write_file = sys.argv[1], sys.argv[2]
-    else:
-        print("Missing required commandline arguments: read_file and write_file")
-        sys.exit(1)
-
+@click.command()
+@click.argument('read_file')
+@click.argument('write_file')
+def main(read_file, write_file):
+    "Commandline program to validate and reformat data from the fly vision camera."
     #  creater an instance of Chunker with the read_file
     try:
         chunker = Chunker(read_file)
@@ -39,10 +34,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # create log files for both FrameValidator and FrameWriter
-    read_log, write_log = create_log_paths(read_file, write_file)
+    log_file = create_log_paths(read_file)
 
-    framevalidator = FrameValidator(read_log, configs["header"], configs["footer"], configs["spacers"])
-    framewriter = FrameWriter(configs["unpack_string"], write_file, write_log)
+    framevalidator = FrameValidator(log_file, configs["header"], configs["footer"], configs["spacers"])
+    framewriter = FrameWriter(configs["unpack_string"], write_file, log_file)
 
     while chunker.chunk_id != "END":
         chunk_id, chunk, byte_loc = chunker.next_chunk()
@@ -54,4 +49,8 @@ if __name__ == "__main__":
 
     chunker.close()
     framewriter.close()
+
+
+if __name__ == "__main__":
+  main() 
  
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Camera Video Reader
-This is the project for reading the raw binary data off the camera data sticks and saving them as usable csv!
+Python commandline script to 
 
 ## Requirements
 Running this programs requires `python3.8` or newer and `pipenv`. If you don't have these installed a guide for their installation can be found [here](https://www.python.org/downloads/) and [here](https://docs.python-guide.org/dev/virtualenvs/#:~:text=Pipenv%20%26%20Virtual%20Environments%20%C2%B6%201%20Make%20sure,installed%20packages%20%C2%B6.%20...%205%20Next%20steps%20%C2%B6). 
