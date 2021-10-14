1b091a0d1b7abb3f378e1320a4c412f1ac775380
Edelsonc <cedelson@iu.edu>
2021-10-14 13:18:17 -0400

    Fixed the example script to actually read the correct IMU location and changed example path

--- example_read_from_res.m
+++ example_read_from_res.m
@@ -1,9 +1,9 @@
 % simple example script of how to read reformatted data into matlab
-file_name = fullfile("C:", "Users", "cedelson", "Desktop", "SD_recordings", "Reformatted_Recordings", "White-noise-recording_reformatted.bin");
+file_name = fullfile("C:", "path", "to", "my", "data.bin");
 data_file = fopen(file_name);
 datau32 = fread(data_file, '*uint32', 'b');
 data_matrix = reshape(datau32, 256, []);
-imu = data_matrix(195:end,:);
+imu = data_matrix(197:217,:);
 imu = imu(1:20,:);
 
 % convert the u32 into it's correct format of i16
@@ -12,4 +12,3 @@ imui16 = reshape(imui16, 20, []);
 
 
 fclose(data_file);
-
